ddb10dd0a3a06198054aab77d34684cacc93ab5b
Azat Khaliullin <AllisonUno@yandex.ru>
2025-10-25 12:00:24 +0400

    Создано приложение блога, откорректированы urls

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    heading = models.CharField(
+        max_length=50,
+        verbose_name="Заголовок",
+        help_text="Введите заголовок статьи",
+    )
+    content = models.TextField(
+        max_length=1000,
+        verbose_name="Содержимое",
+        help_text="Введите содержимое статьи",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите превью статьи",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания статьи",
+        help_text="Введите дату создания",
+        blank=True,
+        null=True,
+    )
+    publication_sign = models.BooleanField(
+        default=False,
+    )
+    views_counter = models.PositiveIntegerField(
+        verbose_name="Счетчик просмотров",
+        help_text="Укажите количество просмотров",
+        default=0,
+    )
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ["heading"]
+
+    def __str__(self):
+        return f"Статья - {self.heading}"
---
+++ blog/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
+
+from blog.models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self):
+        return Blog.objects.filter(publication_sign=True)
+
+    # blog/blog_list.html
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ("heading", "content", "image", "publication_sign", "views_counter")
+    success_url = reverse_lazy("blog:blogs")
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ("heading", "content", "image", "publication_sign", "views_counter")
+    success_url = reverse_lazy("blog:blogs")
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", args=[self.kwargs.get("pk")])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blogs")
